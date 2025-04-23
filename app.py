570c428ba68a48f150c9a34bf0097d9ba468d0b4
Rose Fitzgerald <rcfitzgerald@uiowa.edu>
2025-04-23 16:40:41 -0500

    changed render template

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("Hello World")
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003, debug=True)
